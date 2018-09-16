4c4aa624d6f666814f3c058141dd52cf7aabdee6
Anatoly Belikov <awbelikov@gmail.com>
2018-09-16 14:34:59 +0300

    more fixes python2->python3

--- anglepy/data/mnist.py
+++ anglepy/data/mnist.py
@@ -25,28 +25,32 @@ def load_theano(size=28):
     test_x, test_y = shared_dataset(test)
     return train_x, train_y, valid_x, valid_y, test_x, test_y
 
+
+
 def load_numpy(size=28, binarize_y=False):
     # MNIST dataset
     f = gzip.open(paths[size], 'rb')
-    train, valid, test = pickle.load(f)
+    u = pickle._Unpickler(f)
+    u.encoding = 'latin1'
+    train, valid, test = u.load()
     f.close()
-    train_x, train_y = train
+    train_x, train_y =   train
     valid_x, valid_y = valid
     test_x, test_y = test
     if binarize_y:
         train_y = binarize_labels(train_y)
         valid_y = binarize_labels(valid_y)
         test_y = binarize_labels(test_y)
-        
+
     return train_x.T, train_y, valid_x.T, valid_y, test_x.T, test_y
 
 # Loads data where data is split into class labels
 def load_numpy_split(size=28, binarize_y=False, n_train=50000):
     train_x, train_y, valid_x, valid_y, test_x, test_y = load_numpy(size,False)
-    
+
     train_x = train_x[0:n_train]
     train_y = train_y[0:n_train]
-    
+
     def split_by_class(x, y, num_classes):
         result_x = [0]*num_classes
         result_y = [0]*num_classes
@@ -55,7 +59,7 @@ def load_numpy_split(size=28, binarize_y=False, n_train=50000):
             result_x[i] = x[:,idx_i]
             result_y[i] = y[idx_i]
         return result_x, result_y
-    
+
     train_x, train_y = split_by_class(train_x, train_y, 10)
     if binarize_y:
         valid_y = binarize_labels(valid_y)
@@ -74,7 +78,7 @@ def binarize_labels(y, n_classes=10):
 
 def unbinarize_labels(y):
     return np.argmax(y,axis=0)
-    
+
 def save_reshaped(shape):
     def reshape_digits(x, shape):
         def rebin(a, shape):
@@ -97,7 +101,7 @@ def save_reshaped(shape):
     f = gzip.open(os.path.dirname(__file__)+'/mnist_'+str(shape[0])+'_.pkl.gz','wb')
     pickle.dump((train, valid, test), f)
     f.close()
-    
+
 def make_random_projection(shape):
     W = np.random.uniform(low=-1, high=1, size=shape)
     W /= (np.sum(W**2,axis=1)**(1./2)).reshape((shape[0],1))
@@ -113,7 +117,7 @@ def create_semisupervised(x, y, n_labeled):
     n_x = x[0].shape[0]
     n_classes = y[0].shape[0]
     if n_labeled%n_classes != 0: raise("n_labeled (wished number of labeled samples) not divisible by n_classes (number of classes)")
-    n_labels_per_class = n_labeled/n_classes
+    n_labels_per_class = n_labeled//n_classes
     x_labeled = [0]*n_classes
     x_unlabeled = [0]*n_classes
     y_labeled = [0]*n_classes
@@ -126,5 +130,5 @@ def create_semisupervised(x, y, n_labeled):
         x_unlabeled[i] = x[i][:,idx[n_labels_per_class:]]
         y_unlabeled[i] = y[i][:,idx[n_labels_per_class:]]
     return np.hstack(x_labeled), np.hstack(y_labeled), np.hstack(x_unlabeled), np.hstack(y_unlabeled)
-        
-        
+
+
